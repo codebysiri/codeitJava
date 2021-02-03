6feb4dbbfd245af1ca0f8f2ffe87ff5229c40157
siri <kimss9303@naver.com>
2021-02-03 22:41:21 +0900

    Create Packages

---
+++ Chapter02/src/Midterm/Three/ShapePrinter.java
@@ -0,0 +1,12 @@
+package Midterm.Three;
+
+public class ShapePrinter {
+    public void printTriangle(int height) {
+        for(int i = 1; i <= height; i++) {
+            for(int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
