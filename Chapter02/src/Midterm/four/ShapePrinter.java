7284cf05853b299fb5120c5dec1cf882425ae7f5
siri <kimss9303@naver.com>
2021-02-04 00:02:49 +0900

    Create Packages

---
+++ Chapter02/src/Midterm/four/ShapePrinter.java
@@ -0,0 +1,16 @@
+package Midterm.four;
+
+public class ShapePrinter {
+    public void printPyramid(int height) {
+        for (int i = 1; i <= height; i++) {
+            for (int k = 1; k <= height - i; k++) {
+                System.out.print(" ");
+            }
+            for (int m = 1; m < 2 * i; m++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
