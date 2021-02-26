40932149e0c5cd9d4cd38e0923eba910aec9d963
siri <kimss9303@naver.com>
2021-02-26 22:38:43 +0900

    Create project

---
+++ Chapter02/src/Midterm/Five/Main.java
@@ -0,0 +1,14 @@
+package Midterm.Five;
+
+public class Main {
+    public static void main(String[] args) {
+        ShapePrinter printer = new ShapePrinter();
+
+        // 테스트
+        printer.printFloydsPyramid(3);
+        System.out.println("----------");
+        printer.printFloydsPyramid(5);
+        System.out.println("----------");
+        printer.printFloydsPyramid(15);
+    }
+}
