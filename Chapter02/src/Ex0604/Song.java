2a86c4a0260d45f487d5e890a0da4bcef1eefea1
siri <kimss9303@naver.com>
2021-03-04 21:01:50 +0900

    Create project

---
+++ Chapter02/src/Ex0604/Song.java
@@ -0,0 +1,18 @@
+package Ex0604;
+
+public class Song {
+
+    public final String title;
+    public final Artist artist;
+    public final Album album;
+
+    public Song(String title, Artist artist, Album album) {
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+    }
+
+    public String toString() {
+        return title + " - " +  artist.name + "\n" + album.title + "(" + album.year +")";
+    }
+}
