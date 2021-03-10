ce0064d7d1003309e68cf2aae88149d8bda779dc
siri <kimss9303@naver.com>
2021-03-10 17:48:46 +0900

    Create project

---
+++ Chapter01/src/Ex0310.java
@@ -0,0 +1,11 @@
+public class Ex0310 {
+    public static void main(String[] args) {
+        double koreanScore = 86;
+        double mathScore = 94;
+        double scienceScore = 87;
+        double computerScore = 100;
+        double average = (koreanScore + mathScore + scienceScore + computerScore) / 4;
+
+        System.out.println(average);
+    }
+}
