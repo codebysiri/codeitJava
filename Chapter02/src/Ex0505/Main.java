eb843d9bd7061bf2dc68e8fdb43fb1da9ad05645
siri <kimss9303@naver.com>
2021-03-17 10:55:54 +0900

    Update project

--- Chapter02/src/Ex0505/Main.java
+++ Chapter02/src/Ex0505/Main.java
@@ -6,20 +6,18 @@ public class Main {
     public static int getSurvivingIndex(int n, int k) {
         ArrayList<Integer> soldiers = new ArrayList<>();
 
-        for(int soldierNumber = 1; soldierNumber <= n; soldierNumber++) {
+        for (int soldierNumber = 1; soldierNumber <= n; soldierNumber++) {
             soldiers.add(soldierNumber);
         }
 
         int killIndex = 0;
 
         while (soldiers.size() > 1) {
-            killIndex = (killIndex + k - 1) %soldiers.size();
+            killIndex = (killIndex + k - 1) % soldiers.size();
 
             System.out.println(soldiers.remove(killIndex) + "번 군사가 죽었습니다.");
         }
-
         return soldiers.get(0);
-
     }
 
     public static void main(String[] args) {
